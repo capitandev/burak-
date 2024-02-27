706222cd66a0634828934fc9359d772d16ce63ee
capitandev <sultanovabulkhalik@gmail.com>
2024-02-27 23:51:39 +0900

    Added new controller, libs, and router files, and modified package files

---
+++ src/router.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import app from "./app";
+import memberController from "./controller/memeber.controller";
+const router = express.Router();
+//hompage 
+router.get(`/`,memberController.goHome) 
+//login 
+router.get(`/login`,memberController.getLogin) 
+//sigin up
+router.get(`/siginup`,memberController.getSiginup) 
+export default router;

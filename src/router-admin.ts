a9fb7288376374c40a70084cf17239b79e619015
capitandev <sultanovabulkhalik@gmail.com>
2024-03-13 01:31:30 +0900

    feat:upload files into server via multer

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -30,7 +30,7 @@ routerAdmin.get(
 routerAdmin.post(
   "/product/create",
   restaurantController.verifyRestaurant,
-  makeUploader("products").array("productImages"),
+  makeUploader("products").array("productImages", 5),
   //   uploadProductImage.single("productImage"),
   productController.createNewProduct
 );

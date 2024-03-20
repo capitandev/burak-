f1ecf71ccc181c6358ebbe73ae9135b1673cd83d
capitandev <sultanovabulkhalik@gmail.com>
2024-03-20 10:15:38 +0900

    Fix:

--- src/controller/restaurant.controller.ts
+++ src/controller/restaurant.controller.ts
@@ -53,7 +53,7 @@ restaurantController.processSignup = async (req: AdminRequest, res: Response) =>
   } catch (err) {
     console.log("Error, processSignup", err);
     const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-    res.send(`<script>alert("${message}"); window.location.replace('admin/signup')</script>`);
+    res.send(`<script>alert("${message}"); window.location.replace('/admin/signup')</script>`);
   }
 };
 restaurantController.processLogin = async (req: AdminRequest, res: Response) => {
@@ -68,7 +68,7 @@ restaurantController.processLogin = async (req: AdminRequest, res: Response) => 
   } catch (err) {
     console.log("Error, processLogin", err);
     const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-    res.send(`<script>alert("${message}"); window.location.replace('admin/login')</script>`);
+    res.send(`<script>alert("${message}"); window.location.replace('/admin/login')</script>`);
   }
 };
 restaurantController.logout = async (req: AdminRequest, res: Response) => {

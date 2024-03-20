83d6461b1e734b44d497a6b8cc5d4ae761c44bf0
capitandev <sultanovabulkhalik@gmail.com>
2024-03-21 02:58:34 +0900

    Feat: Create New Product Frontend Logic Develop

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -26,6 +26,7 @@ productController.getAllProducts = async (req: Request, res: Response) => {
 productController.createNewProduct = async (req: AdminRequest, res: Response) => {
   try {
     console.log("createNewProduct");
+    console.log("req:body",);
     if (!req.files?.length) throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
     const data: ProductInput = req.body;
@@ -35,11 +36,11 @@ productController.createNewProduct = async (req: AdminRequest, res: Response) =>
 
     await productService.createNewProduct(data);
 
-    res.send(`<script>alert("Sucessfull creation!"); window.location.replace('admin/product/all')</script>`);
+    res.send(`<script>alert("Sucessfull creation!"); window.location.replace('/admin/product/all')</script>`);
   } catch (err) {
     console.log("Error, createNewProduct", err);
     const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-    res.send(`<script>alert("${message}"); window.location.replace('admin/product/all')</script>`);
+    res.send(`<script>alert("${message}"); window.location.replace('/admin/product/all')</script>`);
   }
 };
 
--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,1 +1,63 @@
 console.log("Products frontend javascript file");
+$(function() {
+    $(".product-collection").on("change", function() {
+        const selectedValue = $(this).val(); // Use $(this) instead of $(".product-collection")
+        if (selectedValue === "DRINK") {
+            $("#product-collection").hide();
+            $("#product-volume").show();
+        } else {
+            $("#product-volume").hide();
+            $("#product-collection").show();
+        }
+    });
+
+    $("#process-btn").on("click", function() {
+        $(".dish-container").slideToggle(500);
+        $(this).hide(); // Hide the button after toggling the .dish-container
+    });
+
+    $("#cancel-btn").on("click", function() {
+        $(".dish-container").slideToggle(500); // Ensure consistent animation duration
+        $("#process-btn").show(); // Show the process button again
+    });
+});
+
+function validateForm() {
+    const productName = $(".product-name").val();
+    const productPrice = $(".product-price").val();
+    const productLeftCount = $(".product-left-count").val();
+    const productCollection = $(".product-collection").val();
+    const productStatus = $(".product-status").val();
+    const productDesc = $(".product-desc").val();
+
+    if (productName === "" || productPrice === "" 
+    || productLeftCount === "" 
+    || productCollection === "" 
+    || productStatus === "" 
+    || productDesc === "") {
+        alert("Please insert all required inputs");
+        return false;
+    } 
+    else return true; 
+}
+
+function previewFileHandler(input, order) {
+    const imgClassName = input.className;
+    console.log("input this:", input);
+    console.log("imgClassName:", imgClassName);
+
+    const file = $(`.${imgClassName}`).get(0).files[0];
+    const fileType = file.type;
+    const validImageType = ["image/jpeg", "image/jpg", "image/png"];
+    if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg, and png files");
+    } else {
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function() {
+                $(`#image-section-${order}`).attr("src", reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+}
